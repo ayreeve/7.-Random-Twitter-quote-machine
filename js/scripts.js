ed9c77e4b365d75a7c616fb77929974c0b4ad871
Kaja <ayreeve@wp.pl>
2017-06-13 13:37:29 +0200

    var names changed

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,40 @@
 // jslint devel: true
 
-var url = 'http://api.icndb.com/jokes/random',
+var $url = 'http://api.icndb.com/jokes/random',
     $button = $('#get-joke').click(function () {
         getJoke();
     }),
     $paragraph = $('#joke'),
-    prefix = "https://cors-anywhere.herokuapp.com/",
-    tweetLink = "https://twitter.com/intent/tweet?text=",
-    quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+    $prefix = "https://cors-anywhere.herokuapp.com/",
+    $tweetLink = "https://twitter.com/intent/tweet?text=",
+    $quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
 function getQuote() {
-    $.getJSON(prefix + quoteUrl, createTweet);
+    $.getJSON($prefix + $quoteUrl, createTweet);
     $.ajaxSetup({
         cache: false
     });
 }
 
 function createTweet(input) {
-    var data = input[0],
+    var $data = input[0],
 
-        quoteText = $(data.content).text().trim(),
-        quoteAuthor = data.title,
+        $quoteText = $($data.content).text().trim(),
+        $quoteAuthor = $data.title,
 
-        tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+        $tweetText = 'Quote of the day - ' + $quoteText + ' Author: ' + $quoteAuthor;
 
-    if (!quoteAuthor.length) {
-        quoteAuthor = "Unknown author";
+    if (!$quoteAuthor.length) {
+        $quoteAuthor = "Unknown author";
     }
 
-    if (tweetText.length > 140) {
+    if ($tweetText.length > 140) {
         getQuote();
     } else {
-        var tweet = tweetLink + encodeURIComponent(tweetText);
-        $('.quote').text(quoteText);
-        $('.author').text("Author: " + quoteAuthor);
-        $('.tweet').attr('href', tweet);
+        var $tweet = $tweetLink + encodeURIComponent($tweetText);
+        $('.quote').text($quoteText);
+        $('.author').text('Author: ' + $quoteAuthor);
+        $('.tweet').attr('href', $tweet);
     }
 }
 
